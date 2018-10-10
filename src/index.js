4d319a5e8ec358077b44fd6eed9013d9d43b6bb3
davjdo <davido_204@hotmail.com>
2018-10-10 23:45:40 +0200

    add redux simple configuration

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import Routes from './routes'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import Routes from './routes';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware)(
+  createStore
+);
 
 const App = () => {
-    return(
-        <BrowserRouter>
-            <Routes/>
-        </BrowserRouter>
-    )
-}
+  return (
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </Provider>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
